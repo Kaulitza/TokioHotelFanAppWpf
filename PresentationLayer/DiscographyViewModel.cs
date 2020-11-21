76d2c3a26aef4d62e7b426c5d86284f3f149d0fc
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-11-21 14:25:24 -0500

    Add files via upload

--- PresentationLayer/DiscographyViewModel.cs
+++ PresentationLayer/DiscographyViewModel.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TokioHotelFanApp.PresentationLayer
 {
     class DiscographyViewModel
     {
+        private void UserProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserProfileView userProfile = new UserProfileView();
+
+            userProfile.Show();
+        }
+        private void Top5Button_Click(object sender, RoutedEventArgs e)
+        {
+            Top5View top5 = new Top5View();
+
+            top5.Show();
+        }
+
+        private void BandInformationButton_Click(object sender, RoutedEventArgs e)
+        {
+            BandInfoView bandInfo = new BandInfoView();
+
+            bandInfo.Show();
+        }
     }
 }
