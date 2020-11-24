b3b8c5eb244b35c37b2f55ac5a583877bc1a11bc
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-11-24 12:31:27 -0500

    Add files via upload

---
+++ BusinessLayer/GameBusiness.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TokioHotelFanApp.DataLayer.Data;
+using TokioHotelFanApp.Models;
+using TokioHotelFanApp.PresentationLayer;
+
+namespace TokioHotelFanApp.BusinessLayer
+{
+
+    public class GameBusiness
+    {
+        public DiscographyViewModel _discographyViewModel;
+        DiscographyView _discographyView;
+        ObservableCollection<Albums> _albums;
+        ObservableCollection<Songs> _songs;
+
+
+        public GameBusiness()
+        {
+            Initializer();
+
+        } 
+        void Initializer()
+        {
+            _albums=GameSeedData.AlbumObjects();
+            _discographyViewModel = new DiscographyViewModel(_albums);
+            _discographyViewModel.SelectedAlbum = _albums.Where(x => x.AlbumName == "Schrei").FirstOrDefault();
+            _discographyViewModel.SearchedSongs = GameSeedData.AllSongs();
+            _discographyView = new DiscographyView(_discographyViewModel);
+            _discographyView.DataContext = _discographyViewModel;
+_discographyView.Show();
+            _discographyViewModel.discographyView = _discographyView;
+
+        }
+    }
+}
