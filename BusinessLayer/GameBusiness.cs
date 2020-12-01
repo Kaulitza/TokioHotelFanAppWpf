312c68423999eace19ac77128f5e7d9a28e7d601
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-12-01 14:24:58 -0500

    Add files via upload

--- BusinessLayer/GameBusiness.cs
+++ BusinessLayer/GameBusiness.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using TokioHotelFanApp.DataLayer.Data;
 using TokioHotelFanApp.Models;
 using TokioHotelFanApp.PresentationLayer;
+using System.Data.SqlClient;
 
 namespace TokioHotelFanApp.BusinessLayer
 {
@@ -14,10 +15,12 @@ namespace TokioHotelFanApp.BusinessLayer
     public class GameBusiness
     {
         public DiscographyViewModel _discographyViewModel;
+        UserProfileViewModel userProfileViewModel;
         DiscographyView _discographyView;
+        UserProfileView profileView;
         ObservableCollection<Albums> _albums;
         ObservableCollection<Songs> _songs;
-
+        Users user;
 
         public GameBusiness()
         {
@@ -26,14 +29,20 @@ namespace TokioHotelFanApp.BusinessLayer
         } 
         void Initializer()
         {
-            _albums=GameSeedData.AlbumObjects();
-            _discographyViewModel = new DiscographyViewModel(_albums);
-            _discographyViewModel.SelectedAlbum = _albums.Where(x => x.AlbumName == "Schrei").FirstOrDefault();
-            _discographyViewModel.SearchedSongs = GameSeedData.AllSongs();
-            _discographyView = new DiscographyView(_discographyViewModel);
-            _discographyView.DataContext = _discographyViewModel;
-_discographyView.Show();
-            _discographyViewModel.discographyView = _discographyView;
+            userProfileViewModel = new UserProfileViewModel();
+            user = new Users();
+            user = GameSeedData.UserData();
+            userProfileViewModel.user_ = user;
+            profileView.DataContext = userProfileViewModel;
+            profileView.Show();
+            //_albums=GameSeedData.AlbumObjects();
+            //_discographyViewModel = new DiscographyViewModel(_albums);
+            //_discographyViewModel.SelectedAlbum = _albums.Where(x => x.AlbumName == "Schrei").FirstOrDefault();
+            //_discographyViewModel.SearchedSongs = GameSeedData.AllSongs();
+            //_discographyView = new DiscographyView(_discographyViewModel);
+            //_discographyView.DataContext = _discographyViewModel;
+            // _discographyView.Show();
+            //_discographyViewModel.discographyView = _discographyView;
 
         }
     }
