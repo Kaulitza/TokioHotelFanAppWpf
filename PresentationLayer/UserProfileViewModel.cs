b9e3211d19c997dbc75be723c5f99fcdfcab6ca9
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-12-01 14:24:21 -0500

    Add files via upload

--- PresentationLayer/UserProfileViewModel.cs
+++ PresentationLayer/UserProfileViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TokioHotelFanApp.Models;
+using TokioHotelFanApp.BusinessLayer;
+
 
 namespace TokioHotelFanApp.PresentationLayer
 {
     class UserProfileViewModel
     {
+        private Users users_;
+        public Users User
+        {
+            get => this.users_;
+            set
+            {
+                this.users_ = value;
+               // this.OnPropertyChanged(nameof(this.User));
+            }
+        }
     }
 }
