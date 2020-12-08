c3364d9d2ba7a3c4529f51139876fcb81ff9a869
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-12-08 10:32:04 -0500

    Add files via upload

--- BusinessLayer/GameBusiness.cs
+++ BusinessLayer/GameBusiness.cs
@@ -16,33 +16,30 @@ namespace TokioHotelFanApp.BusinessLayer
     {
         public DiscographyViewModel _discographyViewModel;
         UserProfileViewModel userProfileViewModel;
-        DiscographyView _discographyView;
-        UserProfileView profileView;
-        ObservableCollection<Albums> _albums;
-        ObservableCollection<Songs> _songs;
-        Users user;
+        public DiscographyView _discographyView;
+        UserProfileView _userProfileView;
+        public ObservableCollection<Albums> _albums;
+        public ObservableCollection<Songs> _songs;
+        SignUpView signUpView = new SignUpView();
+
+
 
         public GameBusiness()
         {
+            SignUpViewInitializer();
             Initializer();
 
         }
+        void SignUpViewInitializer()
+        {
+            signUpView.Show();
+        }
+       
         void Initializer()
         {
-            userProfileViewModel = new UserProfileViewModel();
-            user = new Users();
-            user = GameSeedData.UserData();
-            userProfileViewModel.User = user;
-            profileView.DataContext = userProfileViewModel;
-            profileView.Show();
-            //_albums=GameSeedData.AlbumObjects();
-            //_discographyViewModel = new DiscographyViewModel(_albums);
-            //_discographyViewModel.SelectedAlbum = _albums.Where(x => x.AlbumName == "Schrei").FirstOrDefault();
-            //_discographyViewModel.SearchedSongs = GameSeedData.AllSongs();
-            //_discographyView = new DiscographyView(_discographyViewModel);
-            //_discographyView.DataContext = _discographyViewModel;
-            // _discographyView.Show();
-            //_discographyViewModel.discographyView = _discographyView;
+            
+
+           
 
         }
     }
